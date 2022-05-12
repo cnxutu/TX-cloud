eaa10dcb3e9a422ec7779c6d8817ffc9af8d7c41
xutu <dawn_tx@163.com>
2022-05-12 18:12:19 +0800

    1

---
+++ spring-boot-demo/src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.springbootdemo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//@ServletComponentScan("com.example")
+@MapperScan("com.example.springbootdemo.mybatis.mapper")
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+
+}
